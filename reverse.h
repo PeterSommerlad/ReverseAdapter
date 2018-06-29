6ffbf62833c20f06029b2421c1b1283fa8f833ab
Peter Sommerlad <peter.sommerlad@hsr.ch>
2018-06-29 14:38:06 +0200

    Thomas corrected me

--- reverse.h
+++ reverse.h
@@ -9,9 +9,14 @@ namespace adapter{
 
 template<typename Cont>
 struct reverse{
-	static_assert(std::is_reference_v<Cont>||std::is_move_constructible_v<Cont>,"ref or move constructible");
-	explicit constexpr reverse(Cont &&c)
-	:container{std::forward<Cont>(c)}{}
+	explicit constexpr
+	reverse(Cont & c)
+	:container(c){}
+
+	explicit constexpr
+	reverse(std::remove_reference_t<Cont> &&c)
+	:container(std::move(c)){}
+
 	Cont container;
 	constexpr auto begin() { return std::rbegin(container);}
 	constexpr auto begin() const { return std::rbegin(container);}
@@ -23,7 +28,7 @@ reverse(Cont &) -> reverse<Cont &>;
 template<typename Cont>
 reverse(Cont  &&) -> reverse<Cont>;
 template<typename T>
-reverse(std::initializer_list<T>)-> reverse<std::initializer_list<T>const&>;
+reverse(std::initializer_list<T>)-> reverse<std::initializer_list<T>>;
 
 }
 
